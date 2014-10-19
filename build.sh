905ab6cbda870c58e34b6e2c8c3e07098a305db2
cab404 <me@cab404.ru>
2014-10-19 08:37:10 +0400

    Фикс build.sh.

--- build.sh
+++ build.sh
@@ -2,19 +2,20 @@
 # Собирает все разноцветные версии TabunAC
 
 # В ant.properties должны быть указаны
-# - Параметры keystore-а (jks) в keystore, keystore.password, keystore.alias, keystore.alias.password
+# - Параметры keystore-а (jks) в key.store, key.store.password, key.alias, key.alias.password
 # - Путь к Android SDK в sdk.dir
 
 # Почему ant? Gradle - чёртов слоупок.
 
+rm -rf bin;
 mkdir -p apk;
-cp -f 'res/values/colors.xml' '.colors_cached.xml'
+cp -f 'res/values/colors.xml' '.colors_cached.xml';
 
 for F in $(ls themes | grep -Po ".*(?=\Q.xml\E)")
 do
-	cp -f themes/${F}.xml 'res/values/colors.xml'
-	ant
-	cp 'bin/ac-release.apk' apk/ac-${F}.apk
-done
+	cp -f themes/${F}.xml 'res/values/colors.xml';
+	ant release;
+	cp 'bin/ac-release.apk' apk/ac-${F}.apk;
+done;
 
-mv -f '.colors_cached.xml' 'res/values/colors.xml'
+mv -f '.colors_cached.xml' 'res/values/colors.xml';
