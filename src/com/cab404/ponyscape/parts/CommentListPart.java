86cd7c252056cc2ebfee5e33a4b15f427e6ddfce
cab404 <me@cab404.ru>
2014-11-03 08:10:16 +0300

    Fixed UI lags

--- src/com/cab404/ponyscape/parts/CommentListPart.java
+++ src/com/cab404/ponyscape/parts/CommentListPart.java
@@ -471,9 +471,9 @@ public class CommentListPart extends Part {
 						params.width = nw;
 				}
 
-				listView.scrollTo(offset * comment_ladder, listView.getScrollY());
+//				listView.scrollTo(offset * comment_ladder, listView.getScrollY());
 
-//				Anim.shift(listView, offset * comment_ladder, 100, null);
+				Anim.shift(listView, offset * comment_ladder, 100, null);
 			}
 		}
 
@@ -499,7 +499,7 @@ public class CommentListPart extends Part {
 
 		double scaleComment = Static.cfg.ensure("comments.scale_width", 1.0d);
 		boolean autoshift = Static.cfg.ensure("comments.autoshift", true);
-		int autoshift_offset = Static.cfg.ensure("comments.autoshift.offset", 5);
+		int autoshift_offset = Static.cfg.ensure("comments.autoshift.offset", 1);
 
 		int comment_ladder =
 				(int) (
